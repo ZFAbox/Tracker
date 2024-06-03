c32f5f4fc8ced05760d37e0a1d64df3249efc38c
ZFAbox <starbreeze@yandex.ru>
2024-06-04 01:32:00 +0300

    picker

--- Tracker/Resourses/Colors.swift
+++ Tracker/Resourses/Colors.swift
@@ -16,4 +16,5 @@ extension UIColor {
     
     static var lunchScreeBlue = UIColor.rgbColors(red: 55, green: 114, blue: 231, alpha: 1)
     static var trackerBlack = UIColor.rgbColors(red: 26, green: 27, blue: 34, alpha: 1)
+    static var trackerGray = UIColor.rgbColors(red: 240, green: 240, blue: 240, alpha: 1)
 }
--- Tracker/TabBarViewController.swift
+++ Tracker/TabBarViewController.swift
@@ -43,18 +43,37 @@ final class TabBarViewController: UITabBarController {
         let leftNavigationbuttonImage = UIImage(systemName: "plus")
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftNavigationbuttonImage, style: .plain, target: self, action: #selector(addTarget))
         self.navigationItem.leftBarButtonItem?.tintColor = .trackerBlack
-        var datePicker = UIDatePicker()
+        let rightNavigationLable = UILabel()
+        rightNavigationLable.backgroundColor = .red
+        rightNavigationLable.font = UIFont(name: "SFProDisplay-Regular", size: 17)
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "dd.MM.YY"
+        let date = dateformatter.string(from: Date())
+        rightNavigationLable.text = date
+        let datePicker = UIDatePicker()
 //        let locale = Locale(identifier: "ru")
 //        datePicker.locale = locale
         datePicker.preferredDatePickerStyle = .compact
         datePicker.datePickerMode = .date
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: datePicker)
+
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: datePicker)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: date, style: .plain, target: self, action: #selector(datePickerActivate))
+        self.navigationItem.rightBarButtonItem?.tintColor = .trackerBlack
+        self.navigationItem.rightBarButtonItem?.customView?.layer.backgroundColor = CGColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 1)
+        
     }
     
     @objc func addTarget(){
         print("Добавить цель")
     }
     
+    @objc func datePickerActivate(){
+        let datePicker = UIDatePicker()
+//        let locale = Locale(identifier: "ru")
+//        datePicker.locale = locale
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .automatic
+    }
 }
 
 
