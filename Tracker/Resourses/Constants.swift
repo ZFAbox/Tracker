8455736efcd25b94593b9ad85725910f6e972280
ZFAbox <starbreeze@yandex.ru>
2024-07-19 02:15:11 -0700

    CoreData models included

--- Tracker/Resourses/Constants.swift
+++ Tracker/Resourses/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-enum Weekdays: String {
+enum Weekdays: String, Codable{
     case Sunday = "Воскресенье"
     case Monday = "Понедельник"
     case Tuesday = "Вторник"
