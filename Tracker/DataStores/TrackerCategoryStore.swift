d2e73c4f0b1c83530391cc436a327b7e05cef519
ZFAbox <starbreeze@yandex.ru>
2024-07-26 01:45:35 -0700

    TrackerCategoryStore update

--- Tracker/DataStores/TrackerCategoryStore.swift
+++ Tracker/DataStores/TrackerCategoryStore.swift
@@ -7,30 +7,80 @@
 
 import Foundation
 import CoreData
+import UIKit
 
-final class TrackerCategoryStore {
+final class TrackerCategoryStore: NSObject {
     
     var context: NSManagedObjectContext
+    
+    
+    private lazy var fetchedResultController: NSFetchedResultsController<TrackerCategoryCoreData> = {
+        let fetchRequest = NSFetchRequest<TrackerCategoryCoreData>(entityName: "TrackerCategoryCoreData")
+        let sortDescriptor = NSSortDescriptor(key: "categoryName", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        let fetchedResultController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: #keyPath(TrackerCategoryCoreData.categoryName),
+            cacheName: nil)
+        fetchedResultController.delegate = self
+        try? fetchedResultController.performFetch()
+        return fetchedResultController
+    }()
+    
     
     init(context: NSManagedObjectContext) {
         self.context = context
     }
     
-    convenience init() {
+    convenience override init() {
         self.init(context: (DataStore().persistentContainer.viewContext))
     }
     
-    func saveTrackerCategory(trackerCategory: TrackerCategory, tracker: Tracker) {
+    func saveTrackerCategory(categoryName: String, tracker: Tracker) {
         let trackerRecordData = TrackerCategoryCoreData(context: context)
         let trackerData = TrackerCoreData(context: context)
         trackerData.trackerId = tracker.trackerId
         trackerData.name = tracker.name
         trackerData.emoji = tracker.emoji
-        trackerData.color = tracker.color
+        trackerData.color = UIColor.getHexColor(from: tracker.color)
         trackerData.schedule = tracker.schedule.joined(separator: ",")
-        trackerRecordData.categoryName = trackerCategory.categoryName
+        trackerRecordData.categoryName = categoryName
         trackerRecordData.addToTrackersOfCategory(trackerData)
         saveTrackerCategory()
+    }
+    
+    var numberOfSections: Int {
+        fetchedResultController.sections?.count ?? 0
+    }
+    
+    func numberOfItemsInSection(_ section: Int) -> Int{
+        fetchedResultController.object(at: IndexPath(index: section)).trackersOfCategory?.count ?? 0
+    }
+    
+    func object(_ indexPath: IndexPath) -> TrackerCategory {
+        let trackerCategoryDataCore = fetchedResultController.object(at: indexPath)
+        let trackersOfCategory = trackerCategoryDataCore.trackersOfCategory?.map({$0}) as? [TrackerCoreData]
+        var trackers: [Tracker] = []
+        if let trackersOfCategory = trackersOfCategory {
+            for trackerCoreData in trackersOfCategory {
+                let tracker = Tracker(
+                    trackerId: trackerCoreData.trackerId ?? UUID(),
+                    name: trackerCoreData.name ?? "",
+                    emoji: trackerCoreData.emoji ?? "🤬",
+                    color: UIColor.getUIColor(from: trackerCoreData.color ?? "#FFFFFF"),
+                    schedule: trackerCoreData.schedule?.split(separator: ",") as? [String] ?? ["Воскресенье"])
+                trackers.append(tracker)
+            }
+        }
+            let trackerCategory = TrackerCategory(
+                categoryName: trackerCategoryDataCore.categoryName ?? "None",
+                trackersOfCategory: trackers)
+        return trackerCategory
+    }
+    
+    func addRecord(categoryName: String, tracker: Tracker) {
+        saveTrackerCategory(categoryName: categoryName, tracker: tracker)
     }
     
     private func saveTrackerCategory(){
@@ -40,4 +90,8 @@ final class TrackerCategoryStore {
             print("Ошибка сохранения")
         }
     }
+}
+
+extension TrackerCategoryStore: NSFetchedResultsControllerDelegate {
+    
 }
