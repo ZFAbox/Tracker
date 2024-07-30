9a4f93f1cf47b82c274d92f729e0c0bd9a204c6d
ZFAbox <starbreeze@yandex.ru>
2024-07-31 01:31:09 +0300

    before review

--- Tracker/TrackerCreateView/TrackerCreateViewController.swift
+++ Tracker/TrackerCreateView/TrackerCreateViewController.swift
@@ -29,8 +29,7 @@ class TrackerCreateViewController: UIViewController {
         self.trackerTypeSelectViewController = trackerTypeSelectViewController
         super.init(nibName: nil, bundle: nil)
     }
-    
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
