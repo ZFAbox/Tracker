2f3f21dfa1809fbfad56e10ad82499fd84e6dbc8
ZFAbox <starbreeze@yandex.ru>
2024-08-13 00:13:32 +0300

    updated pageViewController

--- Tracker/Resourses/Colors.swift
+++ Tracker/Resourses/Colors.swift
@@ -16,6 +16,7 @@ extension UIColor {
     
     static var trackerBlue = UIColor.rgbColors(red: 55, green: 114, blue: 231, alpha: 1)
     static var trackerBlack = UIColor.rgbColors(red: 26, green: 27, blue: 34, alpha: 1)
+    static var trackerBlackOpacity30 = UIColor.rgbColors(red: 26, green: 27, blue: 34, alpha: 0.3)
     static var trackerGray = UIColor.rgbColors(red: 240, green: 240, blue: 240, alpha: 1)
     static var trackerGreen = UIColor.rgbColors(red: 51, green: 207, blue: 105, alpha: 1)
     static var trackerOrnage = UIColor.rgbColors(red: 255, green: 136, blue: 30, alpha: 1)
