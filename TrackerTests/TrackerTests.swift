16c5d19985943285a2de8c4879e081f5ae893cd7
ZFAbox <starbreeze@yandex.ru>
2024-10-02 00:06:00 +0300

    updated test for light mode

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -21,8 +21,51 @@ final class TrackerTests: XCTestCase {
         let vc = TrackerViewController(viewModel: viewModel)
         vc.setDatePickerDate(date: date)
         
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
+    
+//    func lightModeTestTrackerController() {
+//        let viewModel = TrackerViewModel()
+//        
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "dd.MM.yyyy"
+//        let date = dateFormatter.date(from: "01.01.2024")!
+//        viewModel.createFixedTrackerForTest()
+//        let vc = TrackerViewController(viewModel: viewModel)
+//        vc.setDatePickerDate(date: date)
+//        
+//        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)), record: true)
+//    }
+    
+    
+//    func testDarkModeTestTrackerController() {
+//        let viewModel = TrackerViewModel()
+//        
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "dd.MM.yyyy"
+//        let date = dateFormatter.date(from: "01.01.2024")!
+//        viewModel.createFixedTrackerForTest()
+//        let vc = TrackerViewController(viewModel: viewModel)
+//        vc.setDatePickerDate(date: date)
+//        
+//        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .dark)), record: true)
+//    }
 
+}
+
+final class TrackerLightModeTest: XCTestCase {
+    
+    func testLightModeTrackerController() {
+        let viewModel = TrackerViewModel()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        let date = dateFormatter.date(from: "01.01.2024")!
+        viewModel.createFixedTrackerForTest()
+        let vc = TrackerViewController(viewModel: viewModel)
+        vc.setDatePickerDate(date: date)
+        
+        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)), record: true)
+    }
 }
 
---
+++ TrackerTestsLightMode/TrackerTestsLightMode.swift
@@ -0,0 +1,11 @@
+//
+//  TrackerTestsLightMode.swift
+//  TrackerTestsLightMode
+//
+//  Created by Федор Завьялов on 01.10.2024.
+//
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+
